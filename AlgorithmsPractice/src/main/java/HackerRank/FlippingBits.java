1a333f726b002cd83f0ebafde9cd262f5a788eab
git <git.com>
2015-02-16 21:25:45 -0700

    flippingbits

---
+++ AlgorithmsPractice/src/main/java/HackerRank/FlippingBits.java
@@ -0,0 +1,33 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class FlippingBits {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long t, a;
+		t = in.nextLong();
+		for (int i = 0; i < t; i++) {
+			a = in.nextLong();
+			String res = flippingBits(a);
+			System.out.println(res);
+		}
+
+	}
+
+	private static String flippingBits(long a) {
+		String binStr = Long.toBinaryString(a);
+		binStr = String.format("%32s", binStr).replace(' ', '0');
+		char[] binArray = binStr.toCharArray();
+		for (int i = 0; i < binArray.length; i++) {
+			if (binArray[i] == '0') {
+				binArray[i] = '1';
+			} else
+				binArray[i] = '0';
+		}
+		binStr = String.valueOf(binArray);
+
+		return Long.valueOf(binStr, 2).toString();
+	}
+}
